41f23e1322dce6fe98bdc5a9d123b63d1a6b7025
arcanjo <luanbrandao4@gmail.com>
2021-06-25 00:55:45 -0300

    feat: add type HttpRequest in AddTaskController

--- src/main/routes/task-routes.ts
+++ src/main/routes/task-routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { DbAddTask } from "../../data/usecases/task/add-task/db-add-task";
 import { SaveTaskMongoRepository } from "../../infra/db/task/task-mongo-repository";
 import { AddTaskController } from "../../presentation/controllers/task/add-task-controller";
+import { HttpRequest } from "../../presentation/protocols";
 
 const saveTaskMongoRepository = new SaveTaskMongoRepository();
 const addTask = new DbAddTask(saveTaskMongoRepository);
@@ -11,8 +12,21 @@ const addTaskController = new AddTaskController(addTask);
 const userRouter = Router();
 
 const fakeAdapter = (controller: any) => {
-    return (req: any, res: any) => {
-         return controller.handle(req, res)
+
+  return async (req: any, res: any) => {
+      const httpRequest: HttpRequest = {
+        body: req.body,
+        params: req.params
+      }
+         const httpResponse = await controller.handle(httpRequest, res);
+
+         if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+          res.status(httpResponse.statusCode).json(httpResponse.body)
+        } else {
+          res.status(httpResponse.statusCode).json({
+            error: httpResponse.body.message
+          })
+        }         
     }
 }
 
--- src/presentation/controllers/task/add-task-controller.ts
+++ src/presentation/controllers/task/add-task-controller.ts
@@ -1,23 +1,24 @@
-import { DbAddTask } from "../../../data/usecases/task/add-task/db-add-task";
 import { AddTask } from "../../../domain/usecases/task/add-task";
-import { Controller } from '../../protocols/controller'
+import { HttpRequest } from "../../protocols";
+import { Controller } from "../../protocols/controller";
 export class AddTaskController implements Controller {
+  constructor(private readonly addTask: AddTask) {}
 
-  constructor(private readonly a: AddTask) {}
-
-  async handle({ req, res }: any): Promise<any> {
+  async handle(req: HttpRequest): Promise<any> {
     try {
       const requestData = {
         title: "Clean Code",
         description: "new test",
       };
 
-      const task = await this.a.add(requestData);
+      const task = await this.addTask.add(requestData);
 
-      return res.status(200).json(task);
+      return {
+        statusCode: 200,
+        body: task,
+      };
     } catch (error) {
       console.log(error);
-      return res.status(500).json(error);
     }
   }
 }
