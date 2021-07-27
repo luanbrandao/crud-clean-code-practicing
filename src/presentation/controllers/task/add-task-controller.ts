351957bc0aa2419d9a0782fdcebb8bb376e7aa21
arcanjo <luanbrandao4@gmail.com>
2021-07-26 23:32:53 -0300

    refactor: add HttpResponse return AddTaskController

--- src/presentation/controllers/task/add-task-controller.ts
+++ src/presentation/controllers/task/add-task-controller.ts
@@ -1,10 +1,11 @@
 import { AddTask } from "../../../domain/usecases/task/add-task";
-import { HttpRequest } from "../../protocols";
+import { ok } from "../../helpers/http/http-helper";
+import { HttpRequest, HttpResponse } from "../../protocols";
 import { Controller } from "../../protocols/controller";
 export class AddTaskController implements Controller {
   constructor(private readonly addTask: AddTask) {}
 
-  async handle(req: HttpRequest): Promise<any> {
+  async handle(req: HttpRequest): Promise<HttpResponse> {
     try {
       const requestData = {
         title: "Clean Code",
@@ -13,12 +14,13 @@ export class AddTaskController implements Controller {
 
       const task = await this.addTask.add(requestData);
 
-      return {
-        statusCode: 200,
-        body: task,
-      };
+      return ok(task)
     } catch (error) {
       console.log(error);
+      return {
+        statusCode: 500,
+        body: { message : 'error'}
+      }
     }
   }
 }
